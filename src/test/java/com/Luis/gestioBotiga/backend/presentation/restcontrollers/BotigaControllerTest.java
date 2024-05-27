b1dee7d062d03ad7487d817537a31eb3f9e596d2
Miles <102043640+MilesVorkosigan@users.noreply.github.com>
2024-05-27 02:00:50 +0200

    Test 2 Errros 1 Failure

--- src/test/java/com/Luis/gestioBotiga/backend/presentation/restcontrollers/BotigaControllerTest.java
+++ src/test/java/com/Luis/gestioBotiga/backend/presentation/restcontrollers/BotigaControllerTest.java
@@ -1,20 +1,27 @@
 package com.Luis.gestioBotiga.backend.presentation.restcontrollers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.util.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import com.Luis.gestioBotiga.backend.presentacio.config.AnswerError;
 import com.Luis.gestioBotiga.backend.presentacio.restcontrollers.BotigaController;
 import com.Luis.gestioBotiga.business.services.BotigaServices;
 import com.Luis.gestioBotiga.bussiness.model.Botiga;
@@ -73,9 +80,74 @@ void obtenir_objecte_per_id() throws Exception{
 		assertThat(responseBody).isEqualToIgnoringWhitespace(respuestaEsperada);
 	
 	}
-	
-	
 
+	@Test
+void solicitamos_botiga_a_partir_de_un_id_inexistente() throws Exception {
+		
+		when(botigaServices.read(1005L)).thenReturn(Optional.empty());
+		
+		MvcResult respuesta = miniPostman.perform(get("/botigues/1005").contentType("application/json"))
+									.andExpect(status().isNotFound())
+									.andReturn();
+		
+		String responseBody = respuesta.getResponse().getContentAsString();
+		String respuestaEsperada = objectMapper.writeValueAsString(new AnswerError("No se encuentra el producto con id 100"));
+		
+		assertThat(responseBody).isEqualToIgnoringWhitespace(respuestaEsperada);
+	}
+
+	@Test
+	void crea_botiga_ok() throws Exception {
+
+		botiga1.setId(null);
+
+		when(botigaServices.create(botiga1)).thenReturn(1033L);
+
+		String requestBody = objectMapper.writeValueAsString(botiga1);
+
+		miniPostman.perform(post("/botigues").content(requestBody).contentType("application/json"))
+				.andExpect(status().isCreated())
+				.andExpect(header().string("Location", "http://localhost/botigues/1033"));
+	}
+
+	@Test
+	void crear_botiga_con_id_NO_NULL() throws Exception{
+		
+		when(botigaServices.create(botiga1)).thenThrow(new IllegalStateException("Problema con el id..."));
+		
+		String requestBody = objectMapper.writeValueAsString(botiga1);
+		
+		MvcResult respuesta = miniPostman.perform(post("/botiga").content(requestBody).contentType("application/json"))
+						.andExpect(status().isBadRequest())
+						.andReturn();
+		
+		String responseBody = respuesta.getResponse().getContentAsString();
+		String respuestaEsperada = objectMapper.writeValueAsString(new AnswerError("Problema con el id..."));
+		
+		assertThat(responseBody).isEqualToIgnoringWhitespace(respuestaEsperada);
+	}
+	@Test
+void eliminamos_producto_ok() throws Exception{
+		
+		miniPostman.perform(delete("/botigues/1033")).andExpect(status().isNoContent());
+		
+		verify(botigaServices, times(1)).delete(1033L);
+	}
+	@Test
+void eliminamos_producto_no_existente() throws Exception{
+		
+		Mockito.doThrow(new IllegalStateException("xxxx")).when(botigaServices).delete(1033L);
+		
+		MvcResult respuesta = miniPostman.perform(delete("/botigues/1033"))
+								.andExpect(status().isNotFound())
+								.andReturn();
+		
+		String responseBody = respuesta.getResponse().getContentAsString();
+		String respuestaEsperada = objectMapper.writeValueAsString(new AnswerError("No se encuentra el producto con id [789]. No se ha podido eliminar."));
+		
+		assertThat(responseBody).isEqualToIgnoringWhitespace(respuestaEsperada);
+		
+	}
 	private void initObjects() {
 		botiga1 = new Botiga();
 		botiga1.setId(100L);
